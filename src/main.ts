bb3ca4a8a971e2aeaf8d82fc4990624ed58b7d9f
Kamil Jankowski <kamil@Kamils-MacBook-Air.local>
2023-10-27 16:33:58 +0200

    changed size of the canvas

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { initializeGame } from './gameLogic'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <canvas style="border: 1px solid black; background: white" height="600" width="600" id="canvas"></canvas>
+    <canvas style="border: 1px solid black; background: white" height="1000" width="1000" id="canvas"></canvas>
   </div>
 `
 
